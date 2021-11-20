1ef17ab3de7f35a730b5417787300f6a4138c991
Maxim Kliashchou <maxim.kliashchou@rdigital.by>
2021-11-20 20:11:53 +0300

    Initial commit

---
+++ src/test/java/bel/mmkle/rentcar/RentCarApplicationTests.java
@@ -0,0 +1,13 @@
+package bel.mmkle.rentcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
