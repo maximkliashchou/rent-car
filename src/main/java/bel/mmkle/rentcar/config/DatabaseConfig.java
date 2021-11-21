14ea576c75ea8cec8c3d46af1009e9ff9454ee7f
Maxim Kliashchou <maxim.kliashchou@rdigital.by>
2021-11-21 17:13:03 +0300

    app postgres db

--- src/main/java/bel/mmkle/rentcar/config/DatabaseConfig.java
+++ src/main/java/bel/mmkle/rentcar/config/DatabaseConfig.java
@@ -2,22 +2,28 @@ package bel.mmkle.rentcar.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.net.URISyntaxException;
 
 
 @Configuration
 public class DatabaseConfig {
-    @Value("${spring.datasource.url}")
-    private String dbUrl;
-
     @Bean
-    public DataSource dataSource() {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(dbUrl);
-        return new HikariDataSource(config);
+    public BasicDataSource dataSource() throws URISyntaxException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        String username = System.getenv("JDBC_DATABASE_USERNAME");
+        String password = System.getenv("JDBC_DATABASE_PASSWORD");
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
     }
 }
